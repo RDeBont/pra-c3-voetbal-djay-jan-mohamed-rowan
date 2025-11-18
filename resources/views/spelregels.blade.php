b18b5a3b84ba7b9f358ba853fd7208e19e86a132
mohamed <D311584@edu.curio.nl>
2025-11-18 10:58:37 +0100

    dropdown spelregelpagina
    dropdown spelregelpagina

--- resources/views/spelregels.blade.php
+++ resources/views/spelregels.blade.php
@@ -3,6 +3,22 @@
     <h1>Spelregels</h1>
 
     <p>Op deze pagina komen de spelregels per toernooi. Deze informatie wordt binnenkort toegevoegd.</p>
+    <div class="dropdowns">
+      <label for="klas">Klas:</label>
+      <select id="klas" name="klas" class="dropdown">
+        <option value="">-- Kies een klas --</option>
+        <option value="klas1">groep 7 </option>
+        <option value="klas2">groep 8</option>
+        <option value="klas3">brugklas</option>
+      </select>
+
+      <label for="spel">Spel:</label>
+      <select id="spel" name="spel" class="dropdown">
+        <option value="">-- Kies een spel --</option>
+        <option value="voetbal">Voetbal</option>
+        <option value="lijnbal">Lijnbal</option>
+      </select>
+    </div>
 
     <ul>
       <li>Voetbaltoernooi – regels volgen KNVB-jeugdregels</li>
