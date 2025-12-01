4885cc0ff732bf32657b74c90ae44860afb88be4
mohamed <D311584@edu.curio.nl>
2025-12-01 15:01:42 +0100

    bugs fix
    bugs fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,40 +1,27 @@
 <x-base-layout>
-  
+
   <main class="homepage">
+    <img src="{{ asset('images/background2.jpg') }}" alt="" class="hero-img">
+
     <section class="hero">
       <h1>Schrijf je school nu in!</h1>
       <p>Doe mee aan spannende schooltoernooien en laat je team schitteren!</p>
-      <a href="{{ url('/inschrijven') }}" class="btn-inschrijven">Inschrijven</a>
+      <a href="{{ url('tournaments') }}" class="btn-tournaments">tournaments</a>
+    </section>
+    <section class="welcome">
+      <h2>Welkom bij de Stichting Paastoernooien Bergen op Zoom!</h2>
+      <p>
+        Al tientallen jaren organiseren wij met veel enthousiasme de Paastoernooien in Bergen op Zoom. Elk voorjaar organiseren we voetbal- en lijnbaltoernooien voor kinderen van groep 3 t/m 8 van de basisschool en de eerste klas van de middelbare school.
+      </p>
+      <p>
+        Scholen uit Bergen op Zoom en omgeving doen enthousiast mee en strijden in verschillende poules om de eer. Voor veel kinderen is het Paastoernooi een vast hoogtepunt van het schooljaar, een dag waarop ze samen met klasgenoten, leerkrachten en ouders genieten van sport en beweging.
+      </p>
+      <p>
+        Op deze website vind je alle informatie die je nodig hebt: van aanmeldformulieren en spelregels tot speelschema’s en uitslagen. Blijf op de hoogte van het laatste nieuws en ontdek hoe jouw school mee kan doen aan onze toernooien.
+      </p>
     </section>
   </main>
 
-  <footer class="site-footer">
-  
-    <div class="footer-bottom">
-      <p>&copy; 2025 Stichting Paastoernooien Bergen op Zoom</p>
-    </div>
 
-    <div class="footer-content">
-      <div class="footer-col">
-        <h3>Bestuur</h3>
-        <p><strong>Voorzitter:</strong> Carin Veringa</p>
-        <p><strong>Secretaris:</strong> Lieke Graumans</p>
-        <p><strong>Penningmeester:</strong> Sander van Kaam</p>
-        <p><strong>Leden:</strong> Stefan Joosen, Nienke Raatgeep, Corné van Tilburg, Kevin Bogers, Orrin van Oosterhout</p>
-      </div>
-
-      <div class="footer-col">
-        <h3>Contact</h3>
-        <p><strong>E-mail:</strong> paastoernooienboz@outlook.com</p>
-        <p><strong>Tel (secretaris):</strong> 06-14605997</p>
-      </div>
-
-      <div class="footer-col">
-        <h3>Gegevens</h3>
-        <p><strong>KvK nummer:</strong> 41106903</p>
-        <p><strong>Rekeningnummer:</strong> NL37 ABNA 0498 5999 73</p>
-      </div>
-    </div>
-  </footer>
 
 </x-base-layout>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,26 +1,34 @@
-<footer class="footer">
-  <h1>Algemene Informatie</h1>
-  <div class="blackLine"></div>
+  <footer class="site-footer">
+    
 
-  <div class="footer-container">
-    <div class="footer-section">
-      <h2>Bedrijfsinformatie</h2>
-      <p>KvK Breda: </p>
+        <div class="footer-bottom">
+          <h2>Algemene Informatie</h2>
+        </div>
+    <div class="footer-content">
+      <div class="footer-col">
+        <h3>Bestuur</h3>
+        <p><strong>Voorzitter:</strong> Carin Veringa</p>
+        <p><strong>Secretaris:</strong> Lieke Graumans</p>
+        <p><strong>Penningmeester:</strong> Sander van Kaam</p>
+        <p><strong>Leden:</strong> Stefan Joosen, Nienke Raatgeep, Corné van Tilburg, Kevin Bogers, Orrin van Oosterhout</p>
+      </div>
+
+      <div class="footer-col">
+        <h3>Contact</h3>
+        <p><strong>E-mail:</strong> paastoernooienboz@outlook.com</p>
+        <p><strong>Tel (secretaris):</strong> 06-14605997</p>
+      </div>
+
+      <div class="footer-col">
+        <h3>Gegevens</h3>
+        <p><strong>KvK nummer:</strong> 41106903</p>
+        <p><strong>Rekeningnummer:</strong> NL37 ABNA 0498 5999 73</p>
+      </div>
+      
+    </div>
+        <div class="footer-bottom">
+      <p>&copy; 2025 Stichting Paastoernooien Bergen op Zoom</p>
     </div>
 
-    <div class="footer-section">
-      <h2>Contactgegevens</h2>
-      <p>E-mail: info@bedrijf.nl</p>
-      <p>Telefoonnummer: 0612345678</p>
-      <p>Adres & Plaats : Roosendaalsebaan 21, Roosendaal</p>
-      <p>Postcode: 4505ZX</p>
-    </div>
-
-  </div>
-  <div class="logo">
-    <a href="{{ url('/') }}">
-      <img src="{{ asset('images/logo.png') }}" alt="Logo" class="logo-img">
-    </a>
-  </div>
-
-</footer>
+    
+  </footer>
