d30f734002c65634bf14a0ca3fdf0f48413a1b5b
Jan Broos <d290008@rocwb.nl>
2025-12-02 09:56:27 +0100

    Loginpage gewijzigd

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\TournamentController;
 use App\Http\Controllers\TournamentCreateController;
-<<<<<<< Updated upstream
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\FixtureController;
 use App\Http\Controllers\inschrijfController;
-
-=======
->>>>>>> Stashed changes
-
+use App\Http\Controllers\FixtureController;
 Route::get('/', function () {
     return view('index');
 });
@@ -35,16 +29,6 @@ Route::get('/spelregels', function () {
     return view('spelregels');
 });
 
-<<<<<<< Updated upstream
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
-
-Route::post('/contact-verzenden', [ContactController::class, 'verzenden'])
-    ->name('contact.verzenden');
-
-=======
->>>>>>> Stashed changes
 Route::get('/dashboard', function () {
     return view('index');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -57,13 +41,13 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('admin', AdminController::class);
-    
+
     Route::post('admin/schools/{id}/accept', [AdminController::class, 'accept'])->name('admin.schools.accept');
     Route::post('admin/schools/{id}/reject', [AdminController::class, 'reject'])->name('admin.schools.reject');
     Route::resource('fixtures', FixtureController::class);
     Route::resource('team', teamController::class);
     Route::resource('users', UserController::class);
-   
+
 
 });
 
